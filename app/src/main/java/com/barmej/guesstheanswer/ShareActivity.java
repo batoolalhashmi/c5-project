a47bb07217df9771770809181ac546ce15bbf685
Batoul <hup_al_eman@hotmail.com>
2019-09-22 13:24:03 +0300

    first commit

---
+++ app/src/main/java/com/barmej/guesstheanswer/ShareActivity.java
@@ -0,0 +1,41 @@
+package com.barmej.guesstheanswer;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class ShareActivity extends AppCompatActivity {
+
+    private String mQuestionText;
+    public EditText mEditTextShare;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        mEditTextShare = findViewById(R.id.edit_Text_share);
+        mQuestionText = getIntent().getStringExtra("question text extra");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("app_pref", MODE_PRIVATE);
+        String questionTitle = sharedPreferences.getString("share_title","");
+        mEditTextShare.setText(questionTitle);
+    }
+    public void onShareQuestionClicked (View view){
+        String questionTitle = mEditTextShare.getText().toString();
+        Intent shareIntent =new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT,questionTitle+"/n" + mQuestionText);
+        shareIntent.setType("text/plain");
+        startActivity(shareIntent);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("app_pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("share_title", questionTitle);
+        editor.apply();
+
+
+    }
+}
